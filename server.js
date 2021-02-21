d66c737bd4d1d234cc4f087d00fc38e5bf10c34c
Michael Goolsby <mgoolsby1091@gmail.com>
2021-02-21 13:51:30 -0500

    re ups for github

---
+++ server.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const { PORT = 3001, NODE_ENV = 'development' } = process.env;
+const express = require('express')
+const app = express();
+
+// Mongo connect
+const mongoose = require('./DB/conn')
+body = require('body-parser')
+
+app.use(body.urlencoded({ extended: false }))
+app.use(body.json())
+
+// Controllers
+const rsvpController = require('./controllers/rsvp.js');
+app.use('/rsvp', rsvpController)
+
+app.get('/', (req, res) => {
+    res.send('navigate to /rsvp to see current list')
+})
+
+app.listen(3001, () => {
+    console.log("Server is up")
+})
