9ac7525f049a2eb6a3689abf333c4976d59dec72
Mike Goolsby <mgoolsby1091@gmail.com>
2021-05-03 16:27:23 -0400

    updates schema to handle guests

--- models/guestList.js
+++ models/guestList.js
@@ -5,7 +5,31 @@ const guestListSchema = mongoose.Schema({
     last_name_root: {type: String, required: true},
     attending: {type: Boolean, required: true},
     food: {type: String},
-    song: String
+    song: String,
+    guest_one: {
+        first_name: {type: String},
+        last_name: {type: String},
+        attending: {type: Boolean},
+        food: {type: String}
+    },
+    guest_two: {
+        first_name: {type: String},
+        last_name: {type: String},
+        attending: {type: Boolean},
+        food: {type: String}
+    },
+    guest_three: {
+        first_name: {type: String},
+        last_name: {type: String},
+        attending: {type: Boolean},
+        food: {type: String}
+    },
+    guest_four: {
+        first_name: {type: String},
+        last_name: {type: String},
+        attending: {type: Boolean},
+        food: {type: String}
+    }
 })
 
 module.exports = mongoose.model('guestList', guestListSchema)
